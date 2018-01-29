2479007dcc571cc6e8817ba62d803d9fd3132d05
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-01-29 16:36:51 -0500

    initial commit

---
+++ Hype Post App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hype Post App
+//
+//  Created by C4Q on 1/29/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

2af94bfcbfb274894e8ee88ca56573caf41bab44
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-01-29 22:37:40 -0500

    add login view Firebase methods

--- Hype Post App/LoginViewController.swift
+++ Hype Post App/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
 class LoginViewController: UIViewController {
 
@@ -19,5 +21,51 @@ class LoginViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+//MARK: -FIREBASE METHODS EXTENSION
+extension LoginViewController {
+    
+    func signUp(withEmail email: String, password pass: String, passwordsMatch bothPassMatch: Bool) {
+        FirebaseAPIClient.manager.createAccount(withEmail: email, and: pass) {(user, error) in
+            guard bothPassMatch == true else {
+                //TODO: ALERT USER PASSWORDS DON'T MATCH
+                return
+            }
+            if Auth.auth().currentUser != nil {
+                FirebaseAPIClient.manager.sendVerificationEmail {(error) in
+                    if error != nil {
+                        print(error!)
+                    } else {
+                        //TODO: NOTIFY USER EMAIL VERIFICATION HAS BEEN SENT
+                    }
+                }
+            }
+        }
+    }
+    
+    func login(withEmail email: String, password pass: String) {
+        //SHOW LOADING INDICATOR HERE
+        
+        FirebaseAPIClient.manager.login(withEmail: email, and: pass) { (user, error) in
+            if error != nil {
+                //TODO: NOTIFY USER OF SIGN IN PROBLEM, DISMISS INDICATOR
+            } else {
+                print("LOGIN SUCCESSFUL")
+                //TODO: SEGUE USER TO MAIN PAGE, DISMISS INDICATOR
+            }
+        }
+    }
+    
+    func resetPassword(withEmail email: String) {
+        FirebaseAPIClient.manager.resetPassword(withEmail: email) { (error) in
+            if error != nil {
+                print(error!)
+                //TODO: NOTIFY USER RESET PASSWORD FAILED
+            } else {
+                //TODO: NOTIFY USER PASSWORD RESET HAS BEEN SET TO EMAIL
+            }
+        }
     }
 }

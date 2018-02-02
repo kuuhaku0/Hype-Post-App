b389bec669507428d52fcf2ed6ba9bc0f9c92764
Caroline Cruz <cruz.caroline08@gmail.com>
2018-02-02 15:18:41 -0500

    AddCommentTAbleViewCell

---
+++ Hype Post App/CustomCells/AddCommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddCommentTableViewCell.swift
+//  Hype Post App
+//
+//  Created by Caroline Cruz on 2/2/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class AddCommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

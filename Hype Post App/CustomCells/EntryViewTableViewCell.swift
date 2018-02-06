53821c133b2df73a9b203507658e6a4e68329e1a
Caroline Cruz <cruz.caroline08@gmail.com>
2018-02-06 18:40:24 -0500

    Add LoginTableViewCell and EntryViewTableViewCell with code

--- Hype Post App/CustomCells/EntryViewTableViewCell.swift
+++ Hype Post App/CustomCells/EntryViewTableViewCell.swift
@@ -10,15 +10,97 @@ import UIKit
 
 class EntryViewTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let constant: CGFloat = 32
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setupViews()
+    }
+    
+    private func setupViews() {
+        prepareSignInButton()
+        prepareCreateAccountButton()
+        setupImageView()
+        setupLabel()
+    }
+    
+    
+    lazy var image: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = #imageLiteral(resourceName: "HypePost")
+        return iv
+    }()
+    
+    lazy var welcomeLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Sign up to experience the Hype."
+        lb.textColor = Color.darkText.primary
+        lb.textAlignment = .center
+        lb.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        return lb
+    }()
+    
+    
+    private func setupLabel() {
+        addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(image.snp.bottom)
+            make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
+            
+        }
+    }
+    
+    
+    
+    private func setupImageView() {
+        addSubview(image)
+        image.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(150)
+            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.25)
+            make.width.equalTo(safeAreaLayoutGuide.snp.width).multipliedBy(0.9)
+            make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
+        }
+    }
+    
+    
+    private func prepareSignInButton() {
+        let btn = RaisedButton(title: "Sign In", titleColor: Color.red.base)
+        btn.addTarget(self, action: #selector(handleSegueToLogInButton(button:)), for: .touchUpInside)
+        
+        
+        self.layout(btn).width(150).height(constant).bottom(300).center().left(40)
+    }
+    
+    /// Handle the resign responder button.
+    @objc
+    internal func handleSegueToLogInButton(button: UIButton) {
+        
+    }
+    
+    private func prepareCreateAccountButton() {
+        let btn = RaisedButton(title: "Create Account", titleColor: Color.blue.base)
+        btn.addTarget(self, action: #selector(handleSegueToCreateAccountButton(button:)), for: .touchUpInside)
+        
+        
+        self.layout(btn).width(150).height(constant).bottom(300).right(40)
+    }
+    
+    /// Handle the resign responder button.
+    @objc
+    internal func handleSegueToCreateAccountButton(button: UIButton) {
+        
     }
 
 }
--- Hype Post App/CustomCells/LoginTableViewCell.swift
+++ Hype Post App/CustomCells/LoginTableViewCell.swift
@@ -10,15 +10,146 @@ import UIKit
 
 class LoginTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let constant: CGFloat = 32
+    
+    func prepareLoginButton() {
+        let btn = RaisedButton(title: "Log In", titleColor: Color.red.base)
+        btn.addTarget(self, action: #selector(handleResignResponderButton(button:)), for: .touchUpInside)
+        
+        self.layout(btn).width(100).height(constant).bottom(300).center()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    /// Handle the resign responder button.
+    @objc
+    internal func handleResignResponderButton(button: UIButton) {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
     }
+    
+    lazy var closeButton: FABButton = {
+        let button = FABButton(image: Icon.cm.close)
+        button.tintColor = Color.red.base
+        button.pulseColor = .gray
+        button.backgroundColor = .clear
+        //        button.addTarget(self, action: #selector(createPost), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var resetPasswordButton: FlatButton = {
+        let button = FlatButton()
+        button.tintColor = .white
+        button.pulseColor = .gray
+        button.titleColor = Color.red.base
+        button.backgroundColor = .clear
+        button.setTitle("Forgot Password", for: .normal)
+        //        button.addTarget(self, action: #selector(createPost), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var createAccountLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Log in to HypePost"
+        lb.textColor = Color.red.base
+        lb.textAlignment = .center
+        lb.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        return lb
+    }()
+    
+    
+    lazy var emailField: ErrorTextField = {
+        let tf = ErrorTextField()
+        tf.placeholder = "Email"
+        tf.detail = "Error, incorrect email"
+        tf.isClearIconButtonEnabled = true
+        tf.isPlaceholderUppercasedWhenEditing = true
+        tf.placeholderAnimation = .hidden
+        return tf
+    }()
+    
+    lazy var passwordField: TextField = {
+        let tf = TextField()
+        tf.placeholder = "Password"
+        tf.clearButtonMode = .whileEditing
+        tf.isVisibilityIconButtonEnabled = true
+        
+        // Setting the visibilityIconButton color.
+        tf.visibilityIconButton?.tintColor = Color.green.base.withAlphaComponent(tf.isSecureTextEntry ? 0.38 : 0.54)
+        return tf
+    }()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setupViews()
+    }
+    
+    private func setupViews() {
+        setupCB()
+        setupLabel()
+        setupEmailTF()
+        setupPasswordTF()
+        setupRPB()
+        prepareLoginButton()
+        
+    }
+    
+    
+    private func setupCB() {
+        addSubview(closeButton)
+        closeButton.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(16)
+            make.leading.equalTo(safeAreaLayoutGuide.snp.leading).offset(16)
+        }
+    }
+    
+    private func setupEmailTF() {
+        addSubview(emailField)
+        emailField.snp.makeConstraints { (make) in
+            make.top.equalTo(createAccountLabel.snp.bottom).offset(50)
+            make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
+            //            make.centerY.equalTo(safeAreaLayoutGuide.snp.centerY)
+            make.width.equalTo(safeAreaLayoutGuide.snp.width).multipliedBy(0.8)
+        }
+    }
+    
+    
+    private func setupPasswordTF() {
+        addSubview(passwordField)
+        passwordField.snp.makeConstraints { (make) in
+            make.width.equalTo(emailField.snp.width)
+            make.top.equalTo(emailField.snp.bottom).offset(40)
+            make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
+        }
+    }
+    
+    private func setupRPB() {
+        addSubview(resetPasswordButton)
+        resetPasswordButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordField.snp.bottom).offset(40)
+            make.leading.equalTo(passwordField.snp.leading)
+        }
+    }
+    
+    private func setupLabel() {
+        addSubview(createAccountLabel)
+        createAccountLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(100)
+            make.centerX.equalTo(safeAreaLayoutGuide.snp.centerX)
+            
+        }
+    }
+    
+
 
 }

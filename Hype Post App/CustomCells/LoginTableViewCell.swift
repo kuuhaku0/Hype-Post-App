5bdacb65647e8787b4dfb0483d067e0ab39b173b
Caroline Cruz <cruz.caroline08@gmail.com>
2018-02-06 18:37:58 -0500

    Add LoginTableViewCell and EntryViewTableViewCell

---
+++ Hype Post App/CustomCells/LoginTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoginTableViewCell.swift
+//  Hype Post App
+//
+//  Created by C4Q on 2/6/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

38f00aa10e31fa7584ee7c07448e9661933c7297
Caroline Cruz <cruz.caroline08@gmail.com>
2018-02-09 10:41:24 -0500

    Constrain AddCommentTextfield and post button

--- Hype Post App/CustomCells/AddCommentViews/TextfieldView.swift
+++ Hype Post App/CustomCells/AddCommentViews/TextfieldView.swift
@@ -13,15 +13,16 @@ class TextfieldView: UIView {
     lazy var textfield: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Write a comment..."
-        tf.borderStyle = .roundedRect
-        tf.layer.borderWidth = 1.3
+//        tf.borderStyle = .roundedRect
+        tf.layer.cornerRadius = 20
+        tf.layer.borderWidth = 0.5
         return tf
     }()
     
     lazy var postButton: UIButton = {
         let but = UIButton ()
         but.setTitle("Post", for: .normal)
-        but.setTitleColor(.black, for: .normal)
+        but.setTitleColor(.gray, for: .normal)
         return but
     }()
     
@@ -49,9 +50,10 @@ class TextfieldView: UIView {
     private func setupButton () {
         addSubview(postButton)
         postButton.snp.makeConstraints { (make) in
-            make.leading.equalTo(textfield.snp.trailing).offset(10)
+            make.leading.equalTo(textfield.snp.trailing).offset(5)
             make.trailing.equalTo(self.snp.trailing).offset(2)
-            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+            //            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+            make.centerY.equalTo(safeAreaLayoutGuide.snp.centerY)
         }
     }
     
@@ -59,9 +61,10 @@ class TextfieldView: UIView {
         addSubview(textfield)
         textfield.snp.makeConstraints { (make) in
             make.leading.equalTo(self.snp.leading).offset(10)
-            make.width.equalTo(self.snp.width).multipliedBy(0.7)
-            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
-            make.height.equalTo(50)
+            make.width.equalTo(self.snp.width).multipliedBy(0.75)
+            //            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+            make.centerY.equalTo(safeAreaLayoutGuide.snp.centerY)
+            make.height.equalTo(40)
         }
     }
     
